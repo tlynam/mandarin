3e44a1d72432049f38a7a3ca0f2d98e3d3e05c81
Todd Lynam <TLynam@gmail.com>
2017-09-23 09:07:10 -1000

    Revert to aws-sdk 2.3

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.5'
 gem 'paperclip'
-gem 'aws-sdk'
+gem 'aws-sdk', '~> 2.3.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,13 @@ Rails.application.configure do
     storage: :s3,
     preserve_files: true,
     s3_host_name: 's3-us-west-2.amazonaws.com',
-    bucket: 'learn-mandarin',
-    region: 'us-west-2'
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    region: ENV.fetch('AWS_REGION'),
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION')
+    }
   }
 end
